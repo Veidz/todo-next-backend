c7aa501bc103c97ca6ead412debc9548f8d2b637
Veidz <joaovictorveidz@gmail.com>
2022-09-03 23:01:45 -0300

    feat: validate date in TaskValidation

--- src/middlewares/task-validation.js
+++ src/middlewares/task-validation.js
@@ -1,3 +1,7 @@
+const { isPast } = require('date-fns');
+
+const TaskModel = require('../models/task');
+
 const TaskValidation = async (req, res, next) => {
   const requiredFields = ['macaddress', 'type', 'title', 'description', 'when'];
 
@@ -6,6 +10,22 @@ const TaskValidation = async (req, res, next) => {
       return res.status(400).json({ error: `${field} is required` });
     }
   }
+
+  if (isPast(new Date(req.body.when))) {
+    return res.status(400).json({ error: 'Date cannot be in the past' });
+  }
+
+  const taskDateExists = await TaskModel.findOne({
+    $and: [
+      { when: new Date(req.body.when) },
+      { macaddress: req.body.macaddress },
+    ],
+  });
+
+  if (taskDateExists) {
+    return res.status(400).json({ error: 'There is already a task for that day and time' });
+  }
+
   return next();
 };
 
